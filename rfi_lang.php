c46bc29e0007463a69c7dd91781f5936422c2553
ZigzagSecurity <40743997+ZigzagSecurity@users.noreply.github.com>
2019-07-20 16:29:42 +0200

    Update rfi_lang.php

--- rfi_lang.php
+++ rfi_lang.php
@@ -1,6 +1,10 @@
 <?php
 echo "debut";
-$homepage = file_get_contents('./index.php');
-echo $homepage;
+$homepage1 = file_get_contents('./en_lang.php');
+$homepage2 = file_get_contents('./fr_lang.php');
+$homepage3 = file_get_contents('./index.php');
+echo $homepage1;
+echo $homepage2;
+echo $homepage3;
 echo "fin";
 ?>
